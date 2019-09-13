3317a56cb497f3b83ac7ed2115c5bdb9490bc615
Jean-Philippe Evrard <jean-philippe@evrard.me>
2019-09-13 13:17:22 +0200

    Do not auto-deploy everything by default with kustomize
    This has multiple risks:
    - When applying kustomize of argocd, the first CRD objects might
      take some time to come up. We should instead wait for their
      readiness before continuing.
    - When our default applications will be a big list, this will
      trigger a massive amount of IOPS, instead of doing this in
      a sync-ed way.
    
    It's still easy to trigger a deployment if auto-deploy is
    present and not synced, by patching the object with a syncPolicy.

--- scripts/setup-autodeployer.sh
+++ scripts/setup-autodeployer.sh
@@ -9,3 +9,26 @@ SCRIPTS_PATH="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))"
 pushd ${SCRIPTS_PATH}/../
     kubectl apply -k argocd-install
     echo "The password for argoCD is:" $(kubectl get -n argocd pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep argocd-server)
+    # The server might take time to start and accept "Apps". Wait a little, until the pods are ready.
+    let podretries=20
+    while [[ "$(kubectl get pods -n argocd | awk '/argocd-server/ {print $3}')" != "Running" ]]; do
+        if (( ${podretries} == 0 )); then
+            echo "Argo server failed to come up on time"
+            exit 1
+        fi
+        sleep 5
+        let podretries--
+    done
+
+    let crdretries=20
+    while [[ "$(kubectl get customresourcedefinition.apiextensions.k8s.io applications.argoproj.io \
+             -o jsonpath='{.status.conditions[?(@.reason=="NoConflicts")].status}' )" != "True" ]]; do
+        if (( ${crdretries} == 0 )); then
+            echo "ArgoCD App CRD fail to come up"
+            exit 1
+        fi
+        sleep 5
+        let crdretries--
+    done
+
+    kubectl apply -f argocd-install/first-app--the-auto-deployer.yaml
