65d6a1ef217ba6521877bbb180f43009c0fde48c
Jean-Philippe Evrard <jean-philippe@evrard.me>
2019-09-13 13:24:17 +0200

    Adapt cleanup tool for ArgoCD

--- scripts/tools/cleanup.sh
+++ scripts/tools/cleanup.sh
@@ -2,34 +2,26 @@
 
 set -x
 
-helm_delete_timeout=${1:-"300"}
-deep_clean=${2:-"no"}
-
-delete_helm_charts(){
-  #defaulting to non empty to avoid mistakes
-  whattodelete=${1:-"deletethis"}
-  helm ls -a | grep $whattodelete | awk 'NR >= 1 {print $1 }' | xargs -r helm delete --timeout ${helm_delete_timeout} --purge $line
-}
+TOOLS_SCRIPTS_PATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 delete_folder_if_exists() {
   #defaulting to non empty to avoid mistakes
   folder=${1:-"deletethis"}
   if [[ -d $folder ]]; then
-      sudo rm -rf $folder
+      rm -rf $folder
   fi
 }
 
-# Purge helm charts
-delete_helm_charts ucp
-delete_helm_charts airship-ingress-kube-system
-delete_helm_charts openstack
 
-sleep 30
+# Remove all apps
+# Triggers finalizers of argocd, to cleanup the app contents.
+kubectl get -n argocd apps -o name | xargs kubectl delete -n argocd
 
-# Delete remnants
-kubectl delete namespace openstack --grace-period=0 --force
-kubectl delete namespace ucp --grace-period=0 --force
-kubectl delete namespace ceph --grace-period=0 --force
+# Delete remnants.
+# This should wait for previous step.
+pushd ${TOOLS_SCRIPTS_PATH}/../../
+    kubectl delete -k argocd-install
+popd
 
 kubectl label node --all openstack-control-plane-
 kubectl label node --all ucp-control-plane-
@@ -43,23 +35,23 @@ delete_folder_if_exists "/var/lib/openstack-helm"
 delete_folder_if_exists "/var/lib/nova"
 rm -f /tmp/socok8s-*
 
-# Deep clean into k8s system, containers cache, and user files.
-if [[ ${deep_clean} == "yes" ]]; then
-    kubectl delete pod -n kube-system --ignore-not-found --grace-period=0 \
-      -l app=ingress-api,application=ingress,component=server --force
-    kubectl delete pod -n kube-system --ignore-not-found --grace-period=0 \
-      -l application=ingress,component=error-pages --force
-    kubectl delete configmap -n kube-system --grace-period=0 --ignore-not-found \
-      airship-ingress-kube-system-nginx-cluster --force
-    #kubectl delete sc --ignore-not-found general --grace-period=0 --force
-    #kubectl delete clusterrolebinding --ignore-not-found PrivilegedRoleBinding
-    #kubectl delete clusterrolebinding --ignore-not-found NonResourceUrlRoleBinding
-    if [[ $(docker images -a | grep "airship" | wc -c) > 0 ]]; then
-        docker images -a | grep "airship" | awk '{print $3}' | xargs -r docker rmi -f
-    fi
-    if [[ -d ${SOCOK8S_WORKSPACE_BASEDIR}/${SOCOK8S_ENVNAME}-workspace/ ]]; then
-        rm -rf ${SOCOK8S_WORKSPACE_BASEDIR}/${SOCOK8S_ENVNAME}-workspace/
-    fi
-    delete_folder_if_exists "/etc/libvirt/qemu/"
-fi
+## Deep clean into k8s system, containers cache, and user files.
+#if [[ ${deep_clean} == "yes" ]]; then
+#    kubectl delete pod -n kube-system --ignore-not-found --grace-period=0 \
+#      -l app=ingress-api,application=ingress,component=server --force
+#    kubectl delete pod -n kube-system --ignore-not-found --grace-period=0 \
+#      -l application=ingress,component=error-pages --force
+#    kubectl delete configmap -n kube-system --grace-period=0 --ignore-not-found \
+#      airship-ingress-kube-system-nginx-cluster --force
+#    #kubectl delete sc --ignore-not-found general --grace-period=0 --force
+#    #kubectl delete clusterrolebinding --ignore-not-found PrivilegedRoleBinding
+#    #kubectl delete clusterrolebinding --ignore-not-found NonResourceUrlRoleBinding
+#    if [[ $(docker images -a | grep "airship" | wc -c) > 0 ]]; then
+#        docker images -a | grep "airship" | awk '{print $3}' | xargs -r docker rmi -f
+#    fi
+#    if [[ -d ${SOCOK8S_WORKSPACE_BASEDIR}/${SOCOK8S_ENVNAME}-workspace/ ]]; then
+#        rm -rf ${SOCOK8S_WORKSPACE_BASEDIR}/${SOCOK8S_ENVNAME}-workspace/
+#    fi
+#    delete_folder_if_exists "/etc/libvirt/qemu/"
+#fi
 
