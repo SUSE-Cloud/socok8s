f3b3cba778001d0744f7446f328fbeb8e561108a
Jean-Philippe Evrard <jean-philippe@evrard.me>
2019-09-13 12:09:29 +0200

    Make auto-deploy tooling a dev tool
    Dev could also need to deploy with kubeconfig file.
    
    Without this patch, the dev has to export it manually, or change
    its own kubeconfig file (link to home for example), to continue
    the deployment.
    
    This makes it easier to use, and reduces the code paths, by making
    CI use the dev script.

--- scripts/dev/autodeploy.sh
+++ scripts/dev/autodeploy.sh
@@ -4,9 +4,9 @@
 
 set -x
 
-CI_SCRIPTS_PATH="$(dirname $(readlink -f "${BASH_SOURCE[0]}" ))"
+DEV_SCRIPTS_PATH="$(dirname $(readlink -f "${BASH_SOURCE[0]}" ))"
 
-pushd ${CI_SCRIPTS_PATH}/../../
+pushd ${DEV_SCRIPTS_PATH}/../../
     # Do not try to deploy on k8s if kubeconfig doesn't exist.
     source scripts/dev/set_kubeconfig.sh && \
     source scripts/setup-autodeployer.sh
